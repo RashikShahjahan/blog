4f663778468ac2b48aa762be8a24a49183cb48c7
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-12-09 15:16:18 -0500

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,7 +8,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: [
       {
